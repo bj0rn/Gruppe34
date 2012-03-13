e40d73576f7bfe85cef85993d6c5d8466a7c8665
Bjørn Tungesvik <meister.bj0rn@gmail.com>
2012-03-13 15:50:41 +0100

    db insert

--- Code/src/no/ntnu/fp/storage/db/Database.java
+++ Code/src/no/ntnu/fp/storage/db/Database.java
@@ -1,5 +1,6 @@
 package no.ntnu.fp.storage.db;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -26,12 +27,76 @@ public abstract class Database {
 		
 		
 	}
-		
+
+	
 	public void insert(Object obj){
-		
+		try{
+
+			Class <? extends Object> clazz = obj.getClass();
+
+			String classname = clazz.getSimpleName();
+
+			StringBuilder sb = new StringBuilder();
+			sb.append("INSERT INTO "+ classname +"(");
+
+			//Get field names
+			Field[] fields = clazz.getDeclaredFields();
+			boolean commaAdded = false;
+			for (Field field : fields) {
+				if(!commaAdded) {
+					commaAdded = true;
+				}
+				else {
+					sb.append(", "); 
+				}
+				sb.append(field.getName());
+			}
+			sb.append(") VALUES(");
+
+			commaAdded = false;
+			//Get field data
+			for(Field field : fields) {
+				String fieldName = field.getName();
+				field.setAccessible(true);
+				Object value = field.get(obj);
+				field.setAccessible(false);
+
+				if(!commaAdded){
+					commaAdded=true;
+				}else{
+					sb.append(", ");
+				}
+				sb.append(parseValue(value));
+			}
+			sb.append(")");
+
+			String query1 = sb.toString();
+
+			connect.createStatement().execute(query1);	
+
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
 	}
-	
-	
+
+	/**
+	 * Create a String object of a inserted Object. Used by the generic
+	 * insert function to manipulate data.
+	 * @param value
+	 * @return
+	 */
+	private Object parseValue(Object value) {
+		return String.format("'%s'", value).toLowerCase();
+	}
+	/**
+	 * C
+	 * @param value
+	 * @return
+	 */
+	private String parseValue(String value) {
+		return String.format("'%s'", value);
+	}
 	
 	
 	//methods
