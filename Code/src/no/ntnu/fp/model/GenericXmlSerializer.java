92f294933f880658024b24d344711e301fe3d6c1
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2012-03-13 17:16:16 +0100

    Oppdatert

--- Code/src/no/ntnu/fp/model/GenericXmlSerializer.java
+++ Code/src/no/ntnu/fp/model/GenericXmlSerializer.java
@@ -2,4 +2,18 @@ package no.ntnu.fp.model;
 
 public class GenericXmlSerializer {
 
+	public String toXml(Object obj) {
+		
+		
+		return null;
+	}
+	
+	public Object fromXml(String xml) {
+		
+		
+		return null;
+		
+	}
+	
+	
 }
