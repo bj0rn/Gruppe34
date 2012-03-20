67f2a10954f48f5f2b69f6af7c6263a78f7b6e31
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2012-03-20 16:54:39 +0100

    XmlSerializer updated

--- Code/src/no/ntnu/fp/model/Meeting.java
+++ Code/src/no/ntnu/fp/model/Meeting.java
@@ -1,29 +1,42 @@
 package no.ntnu.fp.model;
 
+import java.beans.PropertyChangeSupport;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 
 public class Meeting extends CalendarEntry{
 	
+	public enum State {
+		Accepted, Rejected, Pending
+	}
 	
 	private ModelChangeListener modelChangeListener;
-	private List<Participant> participants;
+	
+	private PropertyChangeSupport pcs;
+	
+	private Map<User, State> participants;
 	
 	public Meeting(String description){
 		super(description);
 	}
 	
 	public Meeting(Date start, Date end, String description) {
-		super(start, end, description);
+		super(description, start, end);
 	}
 	
-	public void addParticipant(Participant participant){
-		participants.add(participant);
+	public void addParticipant(User user, State state){
+		participants.put(user, state);
 	}
 
-	public boolean removeParticipant(Participant participant){
-		return participants.remove(participant);
+	public boolean removeParticipant(User user){
+		if (participants.containsKey(user)) {
+			participants.remove(user);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
