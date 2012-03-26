c25a80bd3f91a253e921ae535f01ef8f4f950afb
Andy Philipp <philipp.andy@gmail.com>
2012-03-26 18:29:19 +0200

    updated weekSheet & weekSheetAdapter

--- Code/src/no/ntnu/fp/gui/CalendarPanel.java
+++ Code/src/no/ntnu/fp/gui/CalendarPanel.java
@@ -2,63 +2,72 @@ package no.ntnu.fp.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.List;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import no.ntnu.fp.gui.timepicker.DateModel;
-import no.ntnu.fp.model.Appointment;
+import no.ntnu.fp.model.User;
 
-public class CalendarPanel extends JPanel {
+public class CalendarPanel extends JPanel implements PropertyChangeListener {
 
 	private Navigator weekNavigator;
 	private Navigator yearNavigator;
-
-	JScrollPane scrollArea;
-
 	private WeekSheet weekSheet;
+	private WeekSheetAdapter weekSheetAdapter;
+	JScrollPane scrollArea;
 	private DateModel dateModel;
-	
-	public CalendarPanel() {
-		//Creates instance with current system time Date object
-		this(new DateModel(Calendar.getInstance().getTime()));
-	}
-	
-	public CalendarPanel(DateModel dateModel) {
-		this.dateModel = dateModel;
-		
-		setLayout(new BorderLayout(12,12));
-		
-		JPanel top = new JPanel();
-		top.add(weekNavigator = new Navigator(dateModel.getWeek(), 1, 52));
-		top.add(yearNavigator = new Navigator(dateModel.getYear(), 0, Integer.MAX_VALUE));
+	private User user;
+	private List<User> others;
 
-		weekSheet = new WeekSheet();
-		
+	public CalendarPanel(User user) {
+		// Creates instance with current system time Date object
+		this(new DateModel(Calendar.getInstance().getTime()), user);
+	}
+
+	public CalendarPanel(DateModel dateModel, User user) {
+		setLayout(new BorderLayout(12, 12));
+
+		this.user = user;
+		//TEST
+		dateModel.setWeek(12);
+		//TEST END
+		this.dateModel = dateModel;
+		dateModel.addPropertyChangeListener(this);
+
+		weekNavigator = new Navigator("Week",dateModel.getWeek(), 1, 52);
+		weekNavigator.addPropertyChangeListener(this);
+
+		yearNavigator = new Navigator("Year",dateModel.getYear(), 0, Integer.MAX_VALUE);
+		yearNavigator.addPropertyChangeListener(this);
+
+		JPanel top = new JPanel();
+		top.add(weekNavigator);
+		top.add(yearNavigator);
+
+		weekSheetAdapter = new WeekSheetAdapter(dateModel);
+		weekSheetAdapter.addCalendar(user.getCalendar());
+		weekSheet = new WeekSheet(weekSheetAdapter);
 		scrollArea = new JScrollPane(weekSheet);
-		scrollArea.setPreferredSize(new Dimension(600,300));
+		scrollArea.setPreferredSize(new Dimension(600, 300));
 		scrollArea.setRowHeaderView(new JLabel(" "));
+
 		add(top, BorderLayout.NORTH);
 		add(scrollArea, BorderLayout.CENTER);
 	}
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("WeekSheet");
-		CalendarPanel cp = new CalendarPanel();
-		frame.setContentPane(cp);
-		frame.setSize(1000,600);
-		frame.setVisible(true);
-		cp.weekSheet.addCalendarEntryView(new CalendarEntryView(new Appointment(new Date(2012, 01, 01, 14, 00), new Date(2012, 01, 01, 15, 30), "M�te", 0)));
+	@Override
+	public void propertyChange(PropertyChangeEvent pce) {
+		if (pce.getSource().equals(weekNavigator)) {
+			dateModel.setWeek(weekNavigator.getValue());
+			System.out.println("Select week: " + weekNavigator.getValue());
+		} else if (pce.getSource().equals(yearNavigator)) {
+			dateModel.setYear(yearNavigator.getValue());
+		}
 	}
-
 }
